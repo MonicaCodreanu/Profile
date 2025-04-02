80b3aa01fb1e2196d011cb4d00fa4fce45f29bb8
Monica Codreanu <monica.codreanu@gmail.com>
2025-04-02 18:51:32 +0200

    img update

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
       <h2>Contact me</h2>
       <div className="Contact">
       
-              📧 <strong>Email:</strong> <a href="mailto:monica.codreanu@gmail.com"><img src="mail.png" alt="" /></a><br />
+              📧 <strong>Email:</strong> <a href="mailto:monica.codreanu@gmail.com"><img src="mail.PNG" alt="" /></a><br />
               {/*📞 <strong>Phone:</strong> +46 76 308 3009<br />*/}
               🔗 <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/monicacodreanu">linkedin.com/in/monicacodreanu</a>
             
